abe8a7d8da565e585f22b94ca81e363ab5dbeb56
psyll0n <45129366+psyll0n@users.noreply.github.com>
2024-06-24 12:21:43 +0300

    Modified GreetMe function

--- s01-greet-me-lambda/index.mjs
+++ s01-greet-me-lambda/index.mjs
@@ -40,3 +40,32 @@
 //     console.info("An informative message...");
 //     console.warn("A warning message...");
 // };
+
+const moment =  require('moment');
+const greeting = {
+    "en": "Hello",
+    "fr": "Bonjour",
+    "hi": "Namaste",
+    "es": "Hola",
+    "pt": "Ola",
+    "ur": "Assalamo aleikum",
+    "it": "Ciao",
+    "de": "Hallo",
+    "ar": "Ahlan"
+}
+
+exports.handler = async (event) => {
+    let name = event.pathParameters.name;
+    let {lang, ...info} = event.queryStringParameters;
+
+    let message = `${greeting[lang] ? greeting[lang] : greeting['en'] } ${name}`;
+    let response = {
+        message: message,
+        info: info,
+        timestamp: moment().unix()
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+    }
+}
